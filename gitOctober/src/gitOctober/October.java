179a3c196043a9473e801d316efefb5e44f7163b
jsinger5015 <jsinger5015@001LAB153010820.neisd.ad>
2015-10-20 10:09:19 -0500

    initial Commit

---
+++ gitOctober/src/gitOctober/October.java
@@ -0,0 +1,11 @@
+package gitOctober;
+
+public class October {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Wooo hooo! Its October");
+		System.out.println("And the weather is starting to get windy");
+	}
+
+}
